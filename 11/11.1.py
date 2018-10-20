6065e863be1b23f8d206db4fa4ec3136d967639d
vladgorge <vlad.gorge@gmail.com>
2018-10-20 19:42:52 +0500

    Added exercises number 12

--- 11/11.1.py
+++ 11/11.1.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
 
+def parse_cdp_neighbors(input_txt):
+	f=open(input_txt)
+	devname=f.readline().split('>')[0]
+	f1=f.readlines()[4:]
+	dict_neighbors={}
+	for line in f1:
+		devid=line.replace('Eth ','Eth').replace(' S ','S').split()[0]
+		locint=line.replace('Eth ','Eth').replace(' S ','S').split()[1]
+		portid=line.replace('Eth ','Eth').replace(' S ','S').split()[5]
+		dict_neighbors.setdefault(tuple([devname,locint]))
+		dict_neighbors[tuple([devname,locint])]=tuple([devid,portid])
+		#return dict_neighbors
+	print(dict_neighbors)
+		
+		
+
+		
+parse_cdp_neighbors('sw1_sh_cdp_neighbors.txt')	
+		
+		
+
+'''{('R4', 'Fa0/1'): ('R5', 'Fa0/1'),
+('R4', 'Fa0/2'): ('R6', 'Fa0/0')}'''
